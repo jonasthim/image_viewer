ba4497516e0e7dec79be4682ef195e66ab16cce0
Jari Thorup Palo <jari.palo@ativo.se>
2021-03-19 17:04:07 +0100

    rxjs magic

--- app/server.js
+++ app/server.js
@@ -19,40 +19,53 @@ var fs             = require('fs');
 const Observer     = require('./services/observer');
 var http           = require('http').createServer(app);
 var io             = require('socket.io')(http);
+var rxjs           = require('rxjs');
+var rxjsOperators  = require('rxjs/operators');
 
 var observer = new Observer();
 
 const uploadsFolder = __dirname+"/uploads/";
-
-const fileArray = ["iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="]
+const emptyImg = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
+let fileQueue = [];
+imageTimer = 0;
 
 observer.watchFolder(uploadsFolder);
 
-imageTimer = 0;
+const TIME_TO_DISPLAY = 2000;
+const emitFile = new rxjs.Subject();
+const queueUpdate = new rxjs.Subject();
+
+queueUpdate.subscribe(() => {
+    const queueWaitTimer = rxjs.timer((fileQueue.length - 1) * TIME_TO_DISPLAY);
+    const queueDisplayImageTimer = rxjs.timer(TIME_TO_DISPLAY);
+
+    queueWaitTimer.subscribe((val) => {
+        console.log("START DISPLAY");
+        emitFile.next(fileQueue[0]);
+
+        queueDisplayImageTimer.subscribe(() => {
+            fileQueue.splice(0, 1);
+            if(fileQueue.length > 0) {
+                console.log(fileQueue.length, "DISPLAY NEXT");
+                emitFile.next(fileQueue[0]);
+            } else {
+                console.log("DISPLAY EMPTY");
+                emitFile.next(emptyImg);
+            }
+        });
+    });
+});
+
+observer.on('file-added', file => {
+    // print error message to console
+    fileQueue.push(file.content);
+    queueUpdate.next();
+});
 
 io.on('connection', function(socket) {
-  observer.on('file-added', file => {
-    console.log(imageTimer)
-    // print error message to console
-    fileArray.push(file.content);
-
-    setTimeout(function(){
-      socket.emit('image', fileArray);
-    }, imageTimer);
-    imageTimer = imageTimer - 20000;
-    
-
-    setTimeout(function(){
-      const index = fileArray.indexOf(file.content);
-      if (index > -1) {
-        fileArray.splice(index, 1);
-      }
-      socket.emit('image', fileArray);
-    }, imageTimer + 20000);
-
-  });
-
-
+    emitFile.subscribe((file) => {
+        socket.emit('image', file);
+    });
 });
 
 
@@ -130,39 +143,39 @@ app.get('/auth/twitch/callback', passport.authenticate('twitch', { successRedire
 // Define a simple template to safely generate HTML with values from user's profile
 var uploadTemplate = handlebars.compile(`
     <html>
-        <head>
-            <title>Twitch Auth Sample</title>
-        </head>
-        <body>
-            <form ref='uploadForm' 
-                id='uploadForm' 
-                action='/upload' 
-                method='post' 
-                encType="multipart/form-data"
-            >
-                <input type="file" name="sampleFile" />
-                <input type='submit' value='Upload!' />
-            </form>     
-            <table>
-                <tr>
-                    <th>Access Token</th><td>{{accessToken}}</td>
-                </tr>
-                <tr>
-                    <th>Refresh Token</th><td>{{refreshToken}}</td>
-                </tr>
-                {{#each data}}
-                    <tr>
-                        <th>Display Name</th><td>{{this.display_name}}</td>
-                    </tr>
-                    <tr>
-                        <th>Bio</th><td>{{this.description}}</td>
-                    </tr>
-                    <tr>
-                        <th>Image</th><td><img src="{{this.profile_image_url}}" /></td>
-                    </tr>
-                {{/each}}
-            </table>
-        </body>
+    <head>
+    <title>Twitch Auth Sample</title>
+    </head>
+    <body>
+    <form ref='uploadForm' 
+    id='uploadForm' 
+    action='/upload' 
+    method='post' 
+    encType="multipart/form-data"
+    >
+    <input type="file" name="sampleFile" />
+    <input type='submit' value='Upload!' />
+    </form>     
+    <table>
+    <tr>
+    <th>Access Token</th><td>{{accessToken}}</td>
+    </tr>
+    <tr>
+    <th>Refresh Token</th><td>{{refreshToken}}</td>
+    </tr>
+    {{#each data}}
+    <tr>
+    <th>Display Name</th><td>{{this.display_name}}</td>
+    </tr>
+    <tr>
+    <th>Bio</th><td>{{this.description}}</td>
+    </tr>
+    <tr>
+    <th>Image</th><td><img src="{{this.profile_image_url}}" /></td>
+    </tr>
+    {{/each}}
+    </table>
+    </body>
     </html>`);
 
 const acceptedUsers = process.env.ACCEPTED_USERS.split(" ");
@@ -183,24 +196,24 @@ app.get('/upload', function (req, res) {
     }
     res.send(`
         <html>
-            <head>
-                <title>Twitch Auth Sample</title>
-            </head>
-            <body>
-                <h1>
-                    Tihi
-                </h1>
-            </body>
+        <head>
+        <title>Twitch Auth Sample</title>
+        </head>
+        <body>
+        <h1>
+        Tihi
+        </h1>
+        </body>
         </html>`);
 });
 
 app.post('/upload', function(req, res) {
-  let sampleFile;
-  let uploadPath;
+    let sampleFile;
+    let uploadPath;
 
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
-  }
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   sampleFile = req.files.sampleFile;
@@ -208,45 +221,45 @@ app.post('/upload', function(req, res) {
 
   // Use the mv() method to place the file somewhere on your server
   sampleFile.mv(uploadPath, function(err) {
-    if (err)
-      return res.status(500).send(err);
-    res.sendStatus(200)
+      if (err)
+          return res.status(500).send(err);
+      res.sendStatus(200)
     //res.redirect("/meme");
-  });
+});
 });
 
 var memeTemplate = handlebars.compile(`
     <html>
-        <head>
-            <title>Meme for the pajk</title>
-            <style>
-                body {
-                    max-height: 100vh;
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                }
-                img {
-                    max-height: 100%;
-                }
-            </style>
-            <script src="/socket.io/socket.io.js"></script>
-            <script>
-                var socket = io.connect('/');
-            </script>
-        </head>
-        <body>
-            <script type="text/javascript">
-                setTimeout(() => {
-                    location.reload();
-                }, {{timer}})
-            </script>
-            {{#if src}}
-                <img src="data:image/png;base64, {{src}}" />
-            {{/if}}
-        </body>
+    <head>
+    <title>Meme for the pajk</title>
+    <style>
+    body {
+        max-height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    img {
+        max-height: 100%;
+    }
+    </style>
+    <script src="/socket.io/socket.io.js"></script>
+    <script>
+    var socket = io.connect('/');
+    </script>
+    </head>
+    <body>
+    <script type="text/javascript">
+    setTimeout(() => {
+        location.reload();
+    }, {{timer}})
+    </script>
+    {{#if src}}
+    <img src="data:image/png;base64, {{src}}" />
+    {{/if}}
+    </body>
     </html>
-`);
+    `);
 
 
 
@@ -270,33 +283,33 @@ app.get('/meme', function (req, res) {
 
 app.get('/', function(req, res){
     res.send(`
-            <html>
-                <head>
-                    <title>Meme for the pajk</title>
-                    <style>
-                        body {
-                            max-height: 100vh;
-                            display: flex;
-                            align-items: center;
-                            justify-content: center;
-                        }
-                        img {
-                            max-height: 100%;
-                        }
-                    </style>
-                    <script src="/socket.io/socket.io.js"></script>
-                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-                </head>
-                <body>
-                    <img id="base64image" src="data:image/jpeg;base64, iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" />
-                    <script>
-                        var socket = io.connect('/');
-                        socket.on('image', function(data) {
-                          $("#base64image").attr("src", "data:image/jpeg;base64,"+data[data.length -1]);
-                        });
-                    </script>
-                </body>
-            </html>
+        <html>
+        <head>
+        <title>Meme for the pajk</title>
+        <style>
+        body {
+            max-height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        img {
+            max-height: 100%;
+        }
+        </style>
+        <script src="/socket.io/socket.io.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        </head>
+        <body>
+        <img id="base64image" src="data:image/jpeg;base64, iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" />
+        <script>
+        var socket = io.connect('/');
+        socket.on('image', function(data) {
+            $("#base64image").attr("src", "data:image/jpeg;base64,"+data);
+        });
+        </script>
+        </body>
+        </html>
         `);
 });
 
